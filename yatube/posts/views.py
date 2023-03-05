9003f988dd813827eb04c21dd586c48bfc826207
Kemel Balmukhanov <kemel.sep94@gmail.com>
2023-03-05 15:31:36 +0500

    закончил проект

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,22 @@
-from django.http import HttpResponse
-
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 def index(request):
-    return HttpResponse('def index')
+    posts = Post.objects.order_by('-pub_date')[:10]
+    title = 'Последние обновления на сайте'
+    context = {
+        'posts': posts,
+        'title': title,
+    }
+    return render(request, 'posts/index.html', context)
 
 def group_posts(request, slug):
-    return HttpResponse('def group_posts')
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    title = f'Записи сообщества {group.title}'
+    context = {
+        'group': group,
+        'posts': posts,
+        'title': title,
+    }
+    return render(request, 'posts/group_list.html', context)
